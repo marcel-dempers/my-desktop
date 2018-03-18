5eafc13d82ff36a34d3e0342297d01a691432cdc
marcel-dempers <aimvec@gmail.com>
2018-03-18 22:57:23 +0000

    vscode bashrc install script

---
+++ dockerfiles/vscode/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#pre-requisite
+mkdir -p ~/Applications/contained/vscode/
+#Run vscode
+echo "
+#vscode contained.
+alias vs='xhost local:root
+docker run -d \
+--rm \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $HOME/Applications/contained/vscode/:/home/user \
+-v $HOME/git:/home/user/git \
+-v $HOME/personal/git:/home/user/personal/git \
+-e DISPLAY=unix$DISPLAY \
+--device /dev/dri \
+-v /dev/shm:/dev/shm \
+jess/vscode'
+" >> ~/.bashrc
