4d22723c6a7a03c5d0c5315d1f3619c60cb1d3fd
marcel-dempers <aimvec@gmail.com>
2018-03-19 05:20:13 +0000

    shutter in a container

---
+++ dockerfiles/shutter/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p $HOME/Applications/contained/shutter/
+
+#Run shutter
+echo "
+#shutter contained.
+alias shutter='xhost local:root
+docker run -d \
+--rm \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $HOME/Applications/contained/shutter/.shutter/:/root/.shutter \
+-v $HOME/Applications/contained/shutter/plugins/:/usr/share/shutter/resources/system/plugins \
+-e DISPLAY=unix$DISPLAY \
+-v $HOME/Pictures:/home/root/Pictures \
+--name shutter \
+aimvector/shutter'
+" >> ~/.bashrc
