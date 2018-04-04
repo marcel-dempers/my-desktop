c7451c807fee83559327f08457cad606432f9284
marcel-dempers <aimvec@gmail.com>
2018-04-04 23:52:02 +0000

    peek updated bashrc install script

--- dockerfiles/peek/install.sh
+++ dockerfiles/peek/install.sh
@@ -10,8 +10,14 @@ echo "
 alias peek='xhost local:root
 docker run --rm -d \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v /var/run/dbus:/var/run/dbus \
+--device /dev/video0:/dev/video0 \
+-v /var/run:/var/run \
+--device /dev/dri \
 -v \$HOME/Pictures:/home/root/Pictures \
 -e DISPLAY=unix\$DISPLAY \
 aimvector/peek peek'
 " >> ~/.bashrc
+
+
+
+      
