6a23fe1b078f2da4440f471a97168d6d91a935aa
marcel-dempers <aimvec@gmail.com>
2018-06-18 11:50:11 +1000

    add google cloud

---
+++ dockerfiles/gcloud/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#pre-requisites
+mkdir -p $HOME/Applications/contained/gcloud
+
+#Run gcloud
+echo "
+#gcloud contained.
+alias g='docker run -it \
+--rm \
+-v $HOME/Applications/contained/gcloud:/root/.config/gcloud \
+aimvector/gcloud'
+" >> ~/.bashrc
