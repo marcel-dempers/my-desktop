3f31610e957adcecb094cb847eb712ccc1ba039f
marcel-dempers <aimvec@gmail.com>
2018-07-16 09:50:53 +1000

    add gpick

---
+++ dockerfiles/gpick/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Run gpick
+echo "
+#gpick contained.
+alias gpick='xhost local:root
+docker run -d \
+--rm \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=unix\$DISPLAY \
+aimvector/gpick'
+" >> ~/.bashrc
