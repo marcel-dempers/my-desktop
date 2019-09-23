fec28dc72a8e0df8cb3cb8b2bccc4a215e56627c
marcel-dempers <aimvec@gmail.com>
2019-09-23 11:25:22 +1000

    jupyter

--- dockerfiles/jupyter/install.sh
+++ dockerfiles/jupyter/install.sh
@@ -2,7 +2,7 @@
 
 
 #pre-requisites
-mkdir -p $HOME/Applications/contained/jupyter
+mkdir -p $HOME/containers/jupyter
 
 #Run jupyter
 echo "
@@ -13,9 +13,8 @@ docker run -d \
 --net host \
 -v /etc/localtime:/etc/localtime:ro \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
--v $HOME/Applications/contained/jupyter/:/data \
+-v $HOME/containers/jupyter/:/data \
 -w /data \
 -e DISPLAY=unix\$DISPLAY \
--v $HOME/Downloads:/home/chrome/Downloads \
 aimvector/jupyter'
 " >> ~/.bashrc
